457376d2c84da5de5c16f5a9da2e07809f95f4fe
Paul Song <paulsong24@gmail.com>
2021-06-23 21:31:25 -0400

    stored how many of each letter

--- Chapter_1_Arrays_And_Strings/1.2.py
+++ Chapter_1_Arrays_And_Strings/1.2.py
@@ -1,2 +1,17 @@
 # Check Permutation
 # Given two strings, write a method to decide if one is a permutation of the other.
+
+def checkPermutation(first, second):
+    howManyLetters = {}
+    if len(first) != len(second):
+        return False
+    else:
+        for index, letter in enumerate(first):
+            if letter in howManyLetters:
+                howManyLetters[letter] += 1
+            else:
+                howManyLetters[letter] = 1
+
+
+print(checkPermutation("taco", "coat"))
+print(checkPermutation("dad", "add"))
