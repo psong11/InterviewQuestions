656d1ea7d146fa547e92f24eea856bd92041161f
Paul Song <paulsong24@gmail.com>
2021-06-23 20:51:17 -0400

    1.1 done

--- Chapter_1_Arrays_And_Strings/1.1.py
+++ Chapter_1_Arrays_And_Strings/1.1.py
@@ -1,1 +1,18 @@
-# hello world
+# isUnique
+# Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+def isUnique(str):
+    num = 0
+    existingLetters = {}
+    for index, letter in enumerate(str):
+        if letter in existingLetters:
+            continue
+        else:
+            num += 1
+            existingLetters[letter] = index
+    print(num)
+
+
+isUnique("strawberry")  # should print 8
+isUnique("Paul")  # should print 4
+isUnique("I like it.")  # should print 8
