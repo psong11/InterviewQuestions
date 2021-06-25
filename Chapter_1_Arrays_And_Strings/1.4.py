aa86bed299c84397785c86c7d76c4b062c49300f
Paul Song <paulsong24@gmail.com>
2021-06-25 12:59:27 -0400

    second commit

--- Chapter_1_Arrays_And_Strings/1.4.py
+++ Chapter_1_Arrays_And_Strings/1.4.py
@@ -11,8 +11,8 @@
 def palPerm(str):
     str = str.replace(" ", "")
     str = str.lower()
-    # sort the string
-    # iterate through the string checking if each letter and the letter immediately after it is the same letter
+
+    # count up instances of letters
     letterCount = {}
     for char in str:
         if char in letterCount:
@@ -20,18 +20,13 @@ def palPerm(str):
         else:
             letterCount[char] = 1
 
-    if len(str) % 2 == 0:  # even length
-        for letter in letterCount:
-            if letterCount[letter] % 2 != 0:
-                return False
-        return True
-    else:  # odd length
-        hasOdd = False
-        for value in letterCount.values():
-            if hasOdd:
-                return False
-            if value % 2 != 0:
-                hasOdd = True
+    # if there is more than one letter with an odd number of instances, it's false
+    hasOdd = False
+    for value in letterCount.values():
+        if hasOdd:
+            return False
+        if value % 2 != 0:
+            hasOdd = True
 
     return True
 
