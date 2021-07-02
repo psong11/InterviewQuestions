2ce3c3d7151004f1cc3bd520bc4c00ba5e65d5da
Paul Song <paulsong24@gmail.com>
2021-07-02 16:18:22 -0400

    first implementation

--- Chapter_1_Arrays_And_Strings/1.7.py
+++ Chapter_1_Arrays_And_Strings/1.7.py
@@ -3,5 +3,21 @@
 # Can you do this in place?
 
 # Current Solution
-# Time Complexity:
-# Space Complexity:
+# Time Complexity: O(n^2) where n is the side length of the matrix
+# Space Complexity: O(1) because we are rotating in place.
+
+def rotate(matrix):
+    print()
+    for row in range(len(matrix)):
+        for column in range(row):
+            temp = matrix[row][column]
+            matrix[row][column] = matrix[column][row]
+            matrix[column][row] = temp
+    for row in matrix:
+        row.reverse()
+    return matrix
+
+
+print(rotate([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]))
