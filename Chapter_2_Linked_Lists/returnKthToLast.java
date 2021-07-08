c281ba403e657eb9c90f8ffa4381c5cc7c857c17
Paul Song <paulsong24@gmail.com>
2021-07-07 20:41:09 -0400

    started returnKthToLast

---
+++ Chapter_2_Linked_Lists/returnKthToLast.java
@@ -0,0 +1,19 @@
+/* Return Kth to Last
+Implement an algorithm to find the kth to last element of a singly linked list.
+
+Current Solution
+Time Complexity: 
+Space Complexity:
+
+We could keep track of the "index" of each element in the linked list in like a hashmap, and then get the (length of list - k)th element?
+*/
+
+import java.util.*;
+
+public class returnKthToLast {
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.insert()
+
+    }
+}
